255c5ba7aa504f032a081be4bd197f8debd67db6
Matteo <matteomigliori280489@gmail.com>
2021-04-28 10:20:35 +0200

    milestone 1 ok

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+<?php 
+    include __DIR__.'/database/database.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+
     <title>Document</title>
 </head>
 <body>
@@ -13,9 +18,7 @@
         <img src="img/logo.png" alt="logo">
     </header>
 
-<?php 
-    include __DIR__.'/database/database.php';
-?>
+
 
 <!-- main -->
     <main>
